89134e3003a64f66684c63d0b1f51fa0d90df16e
GinoBeers <582173@student.saxion.nl>
2026-06-10 11:41:56 +0200

    Room Spitting complete?
    added room splitting until rooms are too small to split. with doors

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,75 +1,235 @@
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
+    [Header("Dungeon Settings")]
     [SerializeField]
-    private RectInt dungeonBounds;
+    private RectInt dungeonBounds = new RectInt(0, 0, 100, 50);
 
     [SerializeField]
-    public List<RectInt> ToDo = new();
-    public List<RectInt> Done = new();
+    private int minimumRoomSize = 10;
 
     [SerializeField]
-    private RectInt door;
+    private float generationDelay = 0.1f;
 
-    [ContextMenu("Generate dungeon")]
-    [Button("Generate dungeon", EButtonEnableMode.Playmode)]
-    private void Start()
+    [Header("Debug")]
+    [SerializeField]
+    private List<RectInt> toDo = new();
+
+    [SerializeField]
+    private List<RectInt> done = new();
+
+    [SerializeField]
+    private List<RectInt> doors = new();
+
+    [Button("Generate Dungeon")]
+    private void Generate()
     {
+        StopAllCoroutines();
         StartCoroutine(GenerateDungeon());
     }
 
     private IEnumerator GenerateDungeon()
     {
-        ToDo.Clear();
-        door = RectInt.zero;
-
-        (RectInt roomA, RectInt roomB) = SplitVertically(dungeonBounds);
-        ToDo.Add(roomA);
-        ToDo.Add(roomB);
-
-        RectInt intersection = AlgorithmsUtils.Intersect(roomA, roomB);
-        int randomY = UnityEngine.Random.Range(intersection.y + 1, intersection.y + intersection.height - 1);
-
-        door = new RectInt(intersection.x, randomY, intersection.width, intersection.width);
+        toDo.Clear();
+        done.Clear();
+        doors.Clear();
 
         DebugDrawingBatcher.GetInstance().ClearAllBatchedCalls();
 
-        yield return null;
+        // Start with ONE room
+        toDo.Add(dungeonBounds);
+
+        while (toDo.Count > 0)
+        {
+            RectInt currentRoom = toDo[0];
+            toDo.RemoveAt(0);
+
+            bool canSplitVertically =
+                currentRoom.width >= minimumRoomSize * 2;
+
+            bool canSplitHorizontally =
+                currentRoom.height >= minimumRoomSize * 2;
+
+            // Cannot split anymore
+            if (!canSplitVertically && !canSplitHorizontally)
+            {
+                done.Add(currentRoom);
+
+                DrawDungeon();
+
+                yield return new WaitForSeconds(generationDelay);
+                continue;
+            }
+
+            bool splitVertical = Random.value > 0.5f;
+
+            // Force valid split direction
+            if (!canSplitVertically)
+                splitVertical = false;
+
+            if (!canSplitHorizontally)
+                splitVertical = true;
+
+            RectInt roomA;
+            RectInt roomB;
+
+            // SPLIT
+            if (splitVertical)
+            {
+                (roomA, roomB) = SplitVertically(currentRoom);
+
+                CreateVerticalDoor(roomA, roomB);
+            }
+            else
+            {
+                (roomA, roomB) = SplitHorizontally(currentRoom);
+
+                CreateHorizontalDoor(roomA, roomB);
+            }
+
+            // Add new rooms for processing
+            toDo.Add(roomA);
+            toDo.Add(roomB);
+
+            DrawDungeon();
+
+            yield return new WaitForSeconds(generationDelay);
+        }
+
+        DrawDungeon();
+    }
+
+    private (RectInt, RectInt) SplitVertically(RectInt room)
+    {
+        int splitX = Random.Range(
+            minimumRoomSize,
+            room.width - minimumRoomSize
+        );
+
+        RectInt roomA = new RectInt(
+            room.x,
+            room.y,
+            splitX,
+            room.height
+        );
+
+        RectInt roomB = new RectInt(
+            room.x + splitX - 1,
+            room.y,
+            room.width - splitX + 1,
+            room.height
+        );
+
+        return (roomA, roomB);
+    }
+
+    private (RectInt, RectInt) SplitHorizontally(RectInt room)
+    {
+        int splitY = Random.Range(
+            minimumRoomSize,
+            room.height - minimumRoomSize
+        );
+
+        RectInt roomA = new RectInt(
+            room.x,
+            room.y,
+            room.width,
+            splitY
+        );
+
+        RectInt roomB = new RectInt(
+            room.x,
+            room.y + splitY - 1,
+            room.width,
+            room.height - splitY + 1
+        );
+
+        return (roomA, roomB);
+    }
+
+    private void CreateVerticalDoor(RectInt roomA, RectInt roomB)
+    {
+        RectInt intersection = AlgorithmsUtils.Intersect(roomA, roomB);
+
+        if (intersection.height <= 2)
+            return;
+
+        int randomY = Random.Range(
+            intersection.y + 1,
+            intersection.yMax - 1
+        );
+
+        RectInt door = new RectInt(
+            intersection.x,
+            randomY,
+            1,
+            1
+        );
+
+        doors.Add(door);
+    }
+
+    private void CreateHorizontalDoor(RectInt roomA, RectInt roomB)
+    {
+        RectInt intersection = AlgorithmsUtils.Intersect(roomA, roomB);
+
+        if (intersection.width <= 2)
+            return;
+
+        int randomX = Random.Range(
+            intersection.x + 1,
+            intersection.xMax - 1
+        );
+
+        RectInt door = new RectInt(
+            randomX,
+            intersection.y,
+            1,
+            1
+        );
+
+        doors.Add(door);
+    }
+
+    private void DrawDungeon()
+    {
+        DebugDrawingBatcher.GetInstance().ClearAllBatchedCalls();
 
         DebugDrawingBatcher.GetInstance().BatchCall(() =>
         {
-            // Draw the rooms
-            foreach (var room in ToDo)
+            // Draw rooms still processing
+            foreach (RectInt room in toDo)
             {
-                AlgorithmsUtils.DebugRectInt(roomA, Color.red);
-                RectInt innerRoomA = new RectInt(roomA.x + 1, roomA.y + 1, roomA.width - 2, roomA.height - 2);
-                AlgorithmsUtils.DebugRectInt(innerRoomA, Color.red);
-
-                AlgorithmsUtils.DebugRectInt(roomB, Color.red);
-                RectInt innerRoomB = new RectInt(roomB.x + 1, roomB.y + 1, roomB.width - 2, roomB.height - 2);
-                AlgorithmsUtils.DebugRectInt(innerRoomB, Color.red);
-
+                AlgorithmsUtils.DebugRectInt(room, Color.yellow);
             }
 
-            // Draw the door
-            AlgorithmsUtils.DebugRectInt(door, Color.cyan);
+            // Draw finished rooms
+            foreach (RectInt room in done)
+            {
+                AlgorithmsUtils.DebugRectInt(room, Color.green);
+
+                if (room.width > 2 && room.height > 2)
+                {
+                    RectInt innerRoom = new RectInt(
+                        room.x + 1,
+                        room.y + 1,
+                        room.width - 2,
+                        room.height - 2
+                    );
+
+                    AlgorithmsUtils.DebugRectInt(innerRoom, Color.white);
+                }
+            }
+
+            // Draw doors
+            foreach (RectInt door in doors)
+            {
+                AlgorithmsUtils.DebugRectInt(door, Color.cyan);
+            }
         });
-    }
-    private (RectInt, RectInt) SplitVertically(RectInt pRect)
-    {
-        RectInt roomA = pRect;
-        RectInt roomB = pRect;
-
-        roomA.width = (roomA.width / 2) + UnityEngine.Random.Range(-2, 2);
-        roomB.width -= (roomA.width - 1);
-
-        roomB.x += roomA.width - 1;
-
-        return (roomA, roomB);
     }
 }
