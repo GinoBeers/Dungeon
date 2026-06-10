622778b142f8e10a3387893f32e3cb19bbb0cac6
GinoBeers <582173@student.saxion.nl>
2026-06-10 12:13:02 +0200

    Bounds fix

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -74,7 +74,6 @@ public class DungeonGenerator : MonoBehaviour
             RectInt roomA;
             RectInt roomB;
 
-            // SPLIT
             if (splitVertical)
             {
                 (roomA, roomB) = SplitVertically(currentRoom);
